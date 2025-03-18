72c92a590fb507410d3b9a4dd483308d2a05a90a
Yuvaranei <yuvaranei@gmail.com>
2025-03-18 12:08:20 +0530

    timer with start/pause/resume/reset

---
+++ timer/src/components/Timer/index.jsx
@@ -0,0 +1,51 @@
+import { useRef } from "react";
+import { useEffect } from "react";
+import { useState } from "react"
+
+export default function Timer(){
+    const [timerCount, setTimerCount] = useState(0);
+    const [startTimer, setStartTimer] = useState(false);
+    const [pause, updatePause] = useState(false);
+    const timer = useRef();
+
+    useEffect(() => {
+
+        if(pause){
+            clearInterval(timer.current);
+        }
+        else if(startTimer){
+            timer.current = setInterval(() => (
+                setTimerCount((prevTimerCount) => {
+                    if(prevTimerCount <= 0){
+                        clearInterval(timer.current);
+                        return 0;
+                    }
+                    return prevTimerCount-1;
+                })
+            ), 1000)
+        }
+        
+        if(!startTimer && !pause){
+            clearInterval(timer.current);
+            setTimerCount(0);
+        }
+        
+
+        return () => {
+            clearInterval(timer.current);
+        }
+    }, [startTimer, pause]);
+
+    const minutes = Math.floor(timerCount/60);
+    const seconds = timerCount%60;
+    return (
+        <>
+            <input type="number" onChange={(e) => setTimerCount(e.target.value)} value={timerCount}/>
+            <div>{minutes}:{seconds}</div>
+            <button onClick={() => setStartTimer(true)}>Start</button>
+            <button onClick={() => updatePause(true)}>Pause</button>
+            <button onClick={() => updatePause(false)}>Resume</button>
+            <button onClick={() => setStartTimer(false)}>Reset</button>
+        </>
+    )
+}
